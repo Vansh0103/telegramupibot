15cc0e572a73c47fcc879ecdeb380f78b23af4dd
Vansh0103 <jessieln0103@gmail.com>
2026-04-16 10:58:13 +0530

    Update core.py

--- core.py
+++ core.py
@@ -884,7 +884,7 @@ def send_join_message(chat_id):
         types.InlineKeyboardButton("🔒 Join", url="https://t.me/skullmodder"),
         types.InlineKeyboardButton("🔒 Join", url="https://t.me/botsarefather"),
         types.InlineKeyboardButton("🔒 Join", url="https://t.me/upilootpay"),
-        types.InlineKeyboardButton("🔒 Join", url="https://tinyurl.com/RealUpiLoot"),
+        types.InlineKeyboardButton("🔒 Join", url="https://tinyurl.com/UpiLootpay"),
         ]
     markup.add(*channel_buttons[:2])
     markup.add(*channel_buttons[2:])
